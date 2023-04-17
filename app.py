c9787dc1d8c70b658bed0b3548927300c9a3bf5b
Aderonke Adejare <41582056+AderonkeAdejare@users.noreply.github.com>
2023-04-17 15:13:50 -0400

    First/Basic Implementation of Flask

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 # Python file for Flask 
+
+import os
+
+from flask import Flask
+# from flask import Flask, flash, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+#def index():
+ #   return render_template("index.html")
