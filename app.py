45e0f4ece2153f378fe97db339f9632d82ee2624
Aderonke Adejare <41582056+AderonkeAdejare@users.noreply.github.com>
2023-04-17 15:58:56 -0400

    Connecting app.py with index.html and other HTML pages

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 
 import os
 
-from flask import Flask
-# from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
 # Configure application
 app = Flask(__name__)
@@ -12,7 +12,35 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
-def home():
-    return "Hello, World!"
-#def index():
- #   return render_template("index.html")
+#def home():
+ #   return "Hello, World!"
+def index():
+  return render_template("index.html")
+
+@app.route("/instructions.html")
+def instructions():
+  return render_template("instructions.html")
+
+@app.route("/pause")
+def pause():
+  return render_template("pause.html")
+
+@app.route("/questions")
+def questions():
+  return render_template("questions.html")
+
+@app.route("/results")
+def results():
+  return render_template("results.html")
+
+"""
+def errorhandler(e):
+    # handle errors
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return error(e.name, e.code)
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+ """
